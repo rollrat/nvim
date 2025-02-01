5f9d82463125d7c39820da95df4b4e4940bdf71f
rollrat <rollrat.cse@gmail.com>
2025-02-01 17:59:15 +0900

    init

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      -- 원하는 옵션 설정
+      size = 20,
+      open_mapping = [[<c-\>]],
+      -- ...
+    },
+  },
+}

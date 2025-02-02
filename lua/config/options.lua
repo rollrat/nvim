4a51571fab80c7c05e0da82974870e085cb707b2
rollrat <rollrat.cse@gmail.com>
2025-02-02 12:02:14 +0900

    update win default shell

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,7 @@
 -- Add any additional options here
 vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
 vim.g.snacks_animate = false
+
+if vim.fn.executable("pwsh") then
+  vim.o.shellcmdflag = "/C bash -c zsh"
+end

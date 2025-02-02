2cf7ae43a57ecd0185b8719678c8a816da640300
rollrat <rollrat.cse@gmail.com>
2025-02-02 09:26:19 +0900

    mapping key for git file history

--- README.md
+++ README.md
@@ -3,9 +3,3 @@
 A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
 Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
 
-## Memo
-
-```
-:Telescope git_file_history
-```
-
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,4 +9,9 @@ return {
       },
     },
   },
+  config = function()
+    vim.keymap.set("n", "<leader>ghh", function()
+      require("telescope").extensions.git_file_history.git_file_history()
+    end, { desc = "Git File History" })
+  end,
 }
